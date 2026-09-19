1546cdac513cb7da6d5edf30c08524723e1415e2
mustafoodilov0007-sys <mustafoodilov0007@gmail.com>
2026-09-19 18:50:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv("data/.env")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+BOT_TOKEN = os.getenv("8898088704:AAF7ykAPOrMGZlMWgbR_GPLlX6HfI-hiEaQ")
+ADMIN_ID = int(os.getenv("7741577083", "0"))
 
 # Majburiy obuna uchun kanal (masalan: @kanal_username yoki -100123456789)
 CHANNEL_ID = os.getenv("CHANNEL_ID", "")
